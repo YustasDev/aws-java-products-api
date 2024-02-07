76048d95f4532e8ce9b6be372718efed2a3be2a2
vitaly.gusev <vitaly.gusev@progforce.com>
2024-02-07 18:26:35 +0300

    init 0.3

--- src/main/java/com/serverless/ApiGatewayResponse.java
+++ src/main/java/com/serverless/ApiGatewayResponse.java
@@ -1,18 +1,16 @@
 package com.serverless;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class ApiGatewayResponse {
-
 	private final int statusCode;
 	private final String body;
 	private final Map<String, String> headers;
@@ -48,7 +46,7 @@ public class ApiGatewayResponse {
 
 	public static class Builder {
 
-		private static final Logger LOG = LogManager.getLogger(ApiGatewayResponse.Builder.class);
+		private static final Logger LOG = Logger.getLogger(ApiGatewayResponse.Builder.class);
 
 		private static final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -127,4 +125,6 @@ public class ApiGatewayResponse {
 			return new ApiGatewayResponse(statusCode, body, headers, base64Encoded);
 		}
 	}
+
+
 }
